a18e7408b52ba95e8c8f3e1f9430bffc44bab523
Megan Ansdell <mansdell@users.noreply.github.com>
2018-10-12 05:52:35 +0200

    added inner cavity option + some minor tweaks

--- galario_gaussian.py
+++ galario_gaussian.py
@@ -20,7 +20,7 @@ import time
 # ====================== Parse Arguments ==================
 
 ### example command-line input
-# python galario_gaussian.py 'EPIC_204211116/uvtable_EPIC204211116.txt' 1.33e-3 10.0 0.10 70.0 40.0 -0.1 -0.6 2000 50 --outdir "output"
+# python galario_gaussian.py 'input/uvtable.txt' 1.33e-3 10.0 0.10 70.0 40.0 -0.1 -0.6 2000 50 --outdir "output"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("uvtable", help="name of UV table to fit")
@@ -34,7 +34,8 @@ parser.add_argument("ddec", help="declination offset [arcsec]", type=float)
 parser.add_argument("nsteps", help="number of steps to run mcmc", type=int)
 parser.add_argument("nthreads", help="number of emcee threads to use", type=int)
 parser.add_argument("--outdir", help="output directory; otherwise outputs to current directory")
-parser.add_argument("--restart", help="restart from existing snapshops; input filename of snapshot")
+parser.add_argument("--restart", help="use if restarting from existing snapshops; input filename of snapshot")
+parser.add_argument("--r_in", help="inner radius, if disk has inner cavity [arcsec]", type=float)
 args = parser.parse_args()
 
 
@@ -63,7 +64,11 @@ def lnpostfn(p, p_ranges, rmin, dr, nr, nxy, dxy, u, v, re, im, w):
         return -np.inf
 
     ### unpack the parameters
-    f0, sigma, inc, pa, dra, ddec = p
+    if args.r_in:
+        f0, sigma, r_in, inc, pa, dra, ddec = p
+        r_in *= arcsec
+    else:
+        f0, sigma, inc, pa, dra, ddec = p
 
     ### convert to correct units
     f0 = 10.**f0
@@ -74,7 +79,11 @@ def lnpostfn(p, p_ranges, rmin, dr, nr, nxy, dxy, u, v, re, im, w):
     ddec *= arcsec
 
     ### get gaussian profile
-    f = GaussianProfile(f0, sigma, rmin, dr, nr)
+    f, r = GaussianProfile(f0, sigma, rmin, dr, nr)
+
+    ### add inner hole?
+    if args.r_in:
+        f[r < r_in] *= 0
 
     ### calculate chi-squared
     chi2 = g_double.chi2Profile(f, rmin, dr, nxy, dxy, u, v, re, im, w,
@@ -89,10 +98,13 @@ def GaussianProfile(f0, sigma, rmin, dr, nr):
     """ Gaussian brightness profile. """
 
     ### calculate radial grid
-    R = np.linspace(rmin, rmin + dr * nr, nr, endpoint=False)
+    r = np.linspace(rmin, rmin + dr * nr, nr, endpoint=False)
+
+    ### calculate gaussian profile
+    f = f0 * np.exp(-0.5 * (r / sigma)**2)
 
     ### return gaussian profile
-    return f0 * np.exp(-0.5 * (R / sigma)**2)
+    return f, r
 
 
 # ========================== Code ==========================
@@ -110,12 +122,16 @@ Nxy, Dxy = g_double.get_image_size(U, V, verbose=True, f_max=2.1, f_min=2.0)
 Rmin, dR, nR = 1e-4 * arcsec, 0.01 * arcsec, 2000
 
 ### get initial guesses and ranges of gaussian fit parameters
-p0 = [args.f0, args.sigma, args.incl, args.pa, args.dra, args.ddec]
-p_ranges = [[0.1, 100.0], [0.01, 1.0], [0., 90.], [-180., 180.], [-2.0, 2.0], [-2.0, 2.0]]
+if args.r_in:
+    p0 = [args.f0, args.sigma, args.r_in, args.incl, args.pa, args.dra, args.ddec]
+    p_ranges = [[0.1, 100.], [0.01, 5.], [0.01, 5.], [0., 90.], [0., 180.], [-2., 2.], [-2., 2.]]
+else:
+    p0 = [args.f0, args.sigma, args.incl, args.pa, args.dra, args.ddec]
+    p_ranges = [[0.1, 100.], [0.01, 5.], [0., 90.], [0., 180.], [-2., 2.], [-2., 2.]]
 
 ### setup mcmc
 ndim = len(p0)
-nwalkers = ndim * 6
+nwalkers = ndim * 10
 nsteps = int(args.nsteps / 10)
 tsteps = args.nsteps
 nthreads = args.nthreads
@@ -138,6 +154,12 @@ if args.outdir:
 else:
     outdir = '.'
 
+### set labels for plotting
+if args.r_in:
+    labels = [r"$f_0$", r"$\sigma$", r"$r_{in}$", r"incl", r"PA", r"$\Delta$RA", r"$\Delta$Dec"]
+else:
+    labels = [r"$f_0$", r"$\sigma$", r"incl", r"PA", r"$\Delta$RA", r"$\Delta$Dec"]
+
 ### do mcmc fit
 print("\nStarting fit...\n")
 start = time.time()
@@ -149,7 +171,7 @@ for j in range(nsteps, tsteps + 1, nsteps):
     samples = sampler.chain[:, -500:, :].reshape((-1, ndim))
 
     ### plot corner plot ever nsteps
-    fig = corner.corner(samples, labels=[r"$f_0$", r"$\sigma$", r"$incl$", r"PA", r"$\Delta$RA", r"$\Delta$Dec"], show_titles=True, quantiles=[0.16, 0.50, 0.84], label_kwargs={'labelpad': 20, 'fontsize': 0}, fontsize=8)
+    fig = corner.corner(samples, labels=labels, show_titles=True, quantiles=[0.16, 0.50, 0.84], label_kwargs={'labelpad': 20, 'fontsize': 0}, fontsize=8)
     fig.savefig(os.path.join(outdir, "corner_{}.png".format(j)))
     plt.close('all')
 
@@ -157,17 +179,16 @@ for j in range(nsteps, tsteps + 1, nsteps):
     np.save(os.path.join(outdir, "chain_{}".format(j)), sampler.chain)
     print("{0} steps completed: chain saved in chain_{0}.npy - corner plot saved in triangle_{0}".format(j))
 
-### plot final chains
-samples_final = np.load(os.path.join(outdir, "chain_{}.npy".format(j)))
-labels=[r"$f_0$", r"$\sigma$", r"$incl$", r"PA", r"$\Delta$RA", r"$\Delta$Dec"]
-fig, [axf0, axSig, axInc, axPA, axRA, axDec] = plt.subplots(len(labels), 1, figsize=(9, len(labels) * 2 + 1))
-axes = [axf0, axSig, axInc, axPA, axRA, axDec]
+### save and plot final chains
+np.save(os.path.join(outdir, "chain_{}".format(args.nsteps)), sampler.chain)
+samples_final = np.load(os.path.join(outdir, "chain_{}.npy".format(args.nsteps)))
+fig, axes = plt.subplots(len(labels), 1, figsize=(9, len(labels) * 2 + 1))
 for walker in samples_final:
     for i, (ax, lab) in enumerate(zip(axes, labels)):
         ax.plot(walker[:, i])
         ax.set_xlabel("step number", fontsize=14)
         ax.set_ylabel(lab, fontsize=14)
-fig.savefig(os.path.join(args.outdir, "chain_{}.pdf".format(j)), bbox='tight', rastersize=True, dpi=100)
+fig.savefig(os.path.join(args.outdir, "chain_{}.png".format(j)), bbox='tight', rastersize=True, dpi=100)
 plt.close('all')
 
 ### print out elapsed time
